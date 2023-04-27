d86197092be296f0a54fd2d35d9fea7af1f58c81
Erkin Imakeev <erkinimakeev7@gmail.com>
2023-04-27 18:07:06 +0600

    Added test file

---
+++ src/test/java/com/example/homework60/Homework60ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework60;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework60ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
